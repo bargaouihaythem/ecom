608cab7b45e2026b52f5ee3dab0baf282a6866dc
hbargaoui <haythem.bargaoui@soprahr.com>
2023-07-06 22:24:59 +0100

    ajouter image store

--- EcommerceFrontEnd-Angular-master/src/app/components/home/home.component.ts
+++ EcommerceFrontEnd-Angular-master/src/app/components/home/home.component.ts
@@ -35,7 +35,7 @@ export class HomeComponent implements OnInit {
     responsive: true
   };
   
-  public barChartLabels = [
+ /* public barChartLabels = [
     '2006', '2007', '2008', '2009', '2010', '2011', '2012'
   ];
   
@@ -44,6 +44,7 @@ export class HomeComponent implements OnInit {
   public barChartData = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+  ];*/
+  
 
 }
